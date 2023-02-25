77798e8fbde619876a01fd623ea77b577ca5fe49
Ben Haldenby <benhaldenby@gmail.com>
2023-02-25 16:10:43 +0000

    Add apps to install script

---
+++ apps.sh
@@ -0,0 +1,22 @@
+
+
+# Audio Hijack 4
+# https://rogueamoeba.com/audiohijack/download.php
+# Ben Haldenby
+# STCK-Y4GC-QEFA-EV9C-9NKY-WUP4-GPXR-YPE4-H4MR
+brew install --cask audio-hijack
+
+# SoundSource 5
+# https://rogueamoeba.com/soundsource/download.php
+# Ben Haldenby
+# ZLAT-RKN3-ZA2M-BFWD-8FZJ-DE23-W9XJ-2U8N-DHDA
+brew install --cask soundsource
+
+# Farrago
+# https://rogueamoeba.com/farrago/download.php
+# Ben Haldenby
+# FEBE-QKAW-EMVU-4668-QXKD-G2VY-MBCY-RMJC-24XF
+brew install --cask farrago
+
+# Fission
+brew install --cask fission
