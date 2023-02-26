461b45a3955f0a45fb03b79f7b48ef90635d33af
Ben Haldenby <benhaldenby@gmail.com>
2023-02-26 22:48:28 +0000

    Update ssh.sh

--- setup/ssh.sh
+++ setup/ssh.sh
@@ -41,7 +41,6 @@ echo "Adding github.com and bitbucket.org to known_hosts"
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 ssh-keyscan -H bitbucket.org >> ~/.ssh/known_hosts
 
-
 # Insert SSH config for multiple keys
 echo "Writing SSH config"
 cat << EOF >> ~/.ssh/config
