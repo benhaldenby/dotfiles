fd9d88ee78df8589fde744ae8463635afbc62741
Ben Haldenby <benhaldenby@gmail.com>
2023-03-01 09:56:00 +0000

    Add 1p

--- setup.sh
+++ setup.sh
@@ -10,8 +10,8 @@ COMPUTERNAME="MacBook Pro"
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-  # echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
-  # eval "$(/opt/homebrew/bin/brew shellenv)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
