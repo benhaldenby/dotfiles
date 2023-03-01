5446066e0789fd3be6b3157944ee005389097257
Ben Haldenby <benhaldenby@gmail.com>
2023-03-01 11:17:11 +0000

    Improve 1P int

--- setup/1password.sh
+++ setup/1password.sh
@@ -4,10 +4,10 @@
 echo ""
 echo "🔑 Setting up 1Password SSH keys"
 echo "Enable the SSH agent in 1Password > Preferences > Developer"
-read
+#read
 
 echo "⛔️ Overwrite SSH config? Press any key to continue, or Ctrl+C to cancel"
-read
+#read
 
 echo "🔥 Removing existing SSH config"
 rm -rf ~/.ssh/
@@ -16,27 +16,51 @@ touch ~/.ssh/config
 
 echo "Getting SSH keys from 1Password..."
 echo "Enable 1Password CLI app integration in 1Password > Preferences > Developer"
-read
+#read
 
-# Check if the 1Password CLI app is installed and enabled
-if [ -x "$(command -v op)" ]; then
-  echo "✅ 1Password CLI is installed"
-  # Test if the 1Password CLI app is integrated with the account
-  if op list items >/dev/null 2>&1; then
-    # If the script reaches this point, the 1Password CLI app is installed and integrated
-    echo "✅ 1Password CLI app integration is installed and enabled"
-    # read
-  else
-    echo "🚫 1Password CLI app is not installed"
-    echo "Signing out of a forgetting 1Password accounts..."
-    op account forget --all
-    echo "Now add the accounts and signin manually..."
-    
-    # Add the accounts and signin manually
-    eval $(op account add --address my.1password.com --email benhaldenby@gmail.com --secret-key A3-LHLCST-8MZPL3-QESHG-STNG4-Z82AW-9G79W --shorthand ben --signin)
-    eval $(op account add --address matrixcreate.1password.com --email ben@matrixcreate.com  --secret-key A3-ZNK6LH-G93L36-FS982-3VH9G-6AMHF-ZSHXV --shorthand matrix --signin)
-  fi
-fi
+
+read -r -p "Sign and remove accounts? [Y/n] " input
+ 
+case $input in
+      [yY][eE][sS]|[yY])
+            echo "Sign out of all accounts"
+            op signout --all
+            echo "Forget of all accounts"
+            op account forget --all
+            ;;
+      [nN][oO]|[nN])
+            echo "You say No"
+            ;;
+      *)
+            echo "Invalid input..."
+            exit 1
+            ;;
+esac
+
+
+
+# TODO: Find a method of directly testing for 1Password CLI app integration
+# Prompt user to choose between 1Password CLI app integration and manual signin
+PS3='Please enter your choice: '
+options=("Use 1Password CLI desktop app integration" "Sign in manually")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Use 1Password CLI desktop app integration")
+            break
+            ;;
+
+        "Sign in manually")
+            echo "Sign in manually..."
+            # Add the accounts and signin manually
+            eval $(op account add --address my.1password.com --email benhaldenby@gmail.com --secret-key A3-LHLCST-8MZPL3-QESHG-STNG4-Z82AW-9G79W --shorthand ben --signin)
+            eval $(op account add --address matrixcreate.1password.com --email ben@matrixcreate.com  --secret-key A3-ZNK6LH-G93L36-FS982-3VH9G-6AMHF-ZSHXV --shorthand matrix --signin)
+
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
 
 
 # After you sign in, save your session token to an environment variable. 
