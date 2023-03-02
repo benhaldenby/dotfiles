f7490cc290b42187f5e8bdfc80fc5ff2c1323c2f
Ben Haldenby <benhaldenby@gmail.com>
2023-03-02 12:51:23 +0000

    Lots of fixes

--- setup/1password.sh
+++ setup/1password.sh
@@ -8,47 +8,52 @@ echo "Enable SSH Agent and 1Password CLI in 1Password > Preferences > Developer"
 echo "If you don't have 1Password CLI enabled, you'll need to sign in manually"
 read
 
-# Create the .ssh directory if it doesn't exist
+# Create ssh directory and base config if they don't already exist
 mkdir -p ~/.ssh && chmod 700 ~/.ssh
 touch ~/.ssh/config
 chmod 600 ~/.ssh/config
 
-# TODO: When is it necessary to signout of all accounts?
-#op signout --all
-#op account forget --all
-
 # Prompt user to choose between 1Password CLI app integration and manual signin
 # TODO: Find a method of directly testing for availablity of 1Password CLI app integration
 options=("Use 1Password CLI desktop app integration" "Sign in manually...")
-select opt in "${options[@]}"
+select option in "${options[@]}"
 do
-  case $opt in
+  case $option in
     "Use 1Password CLI desktop app integration")
-      eval $(op signin)  # this will trigger the next account wizard if there are no accounts
-      sleep 1
+      # Check if 1Password CLI is already signed in
+      # Sign in to 1Password CLI
+
+      # Sign in to 1Password CLI
+      # This will trigger the next account wizard if there are no accounts
+      eval $(op signin)
+      # Dramatic pause
+      sleep 1 
+      # Show currently signed in account
       op whoami
       echo ""
       break
       ;;
 
     "Sign in manually...")
-      # Sign in to 1Password CLI
-
+      # Get list of known accounts
       ACCOUNTS=$(op account list)  # run the command and capture its output
-      if [ -z "$ACCOUNTS" ]; then  # check if the output is empty
+      # No accounts are known, so sign in
+      if [ -z "$ACCOUNTS" ]; then   # checks if the output is empty
         eval $(op signin)
       else
-        echo "Available accounts:"
+        # List known accounts
+        echo "Known accounts:"
         op account list
+
         read -p "Add another account? [y/N]" ADDACCOUNT
         if [[ $ADDACCOUNT =~ ^[Yy]$ ]]; then
           # Add another account
           eval $(op account add --signin)
         else
+          # Sign in to 1Password CLI – displays the interactive account prompt
           eval $(op signin)
         fi  
       fi
-
       break
       ;;
     *) echo "invalid option $REPLY";
@@ -59,36 +64,35 @@ do
   op whoami
 done
 
-# Ask for a FILENAME for the new ssh keys
-echo "Enter the vault and item name for the SSH keys you want to use"
-
+# Ask user to choose a vault (will depend on Personal or Teams/Business account)
+echo "Choose a vault..."
 # Select a vault name
-#echo "Choose a vault:"
-PS3="Choose a vault: "
+#PS3="Choose a vault: "
 options=("private" "personal")
-select opt in "${options[@]}"
+select option in "${options[@]}"
 do
-    case $opt in
-        "private")
-            VAULTNAME="private"
-            break
-            ;;
+  case $option in
+    "private")
+      VAULTNAME="private"
+      break
+      ;;
 
-        "personal")
-            VAULTNAME="personal"
-            break
-            ;;
-        *) echo "invalid option $REPLY";
-    esac
+    "personal")
+      VAULTNAME="personal"
+      break
+      ;;
+    *) echo "Invalid option $REPLY";
+  esac
 done
 
-# Select an item name
+# Ask user to select an item name, defaults to 'sshkey'
 read -p "Enter an item: [sshkey]" ITEMNAME
 if ITEMNAME=""; then
   ITEMNAME="sshkey"
 fi
 
-echo "Enter an alias to use for the filename and config file (leave blank for standard: id_rsa)"
+# Ask user to choose an alias for the filename and config file
+echo "Enter an alias to use for the SSH key filename and config file (leave blank for standard: id_rsa)"
 read ALIAS
 
 # Set a standard id_rsa filename, or use the ALIAS if it was set
@@ -105,22 +109,17 @@ fi
 # Get the private and public keys from 1Password, and save them to ~/.ssh
 op read "op://$VAULTNAME/$ITEMNAME/privatekey" > ~/.ssh/$FILENAME
 op read "op://$VAULTNAME/$ITEMNAME/publickey" > ~/.ssh/$FILENAME.pub
-# set read/write permissions for current user only
+
+# Set read/write permissions for current user only
 chmod 600 ~/.ssh/$FILENAME
 chmod 600 ~/.ssh/$FILENAME.pub
 
-#echo op read "op://personal/sshkey/privatekey"
-#echo op read "op://$VAULTNAME/sshkey/privatekey"
-#cat ~/.ssh/$FILENAME.pub
-
-# Prompt the user for confirmation
+# Ask the user if they want to write out known_hosts file
 read -p "Add hosts to ~/.ssh/known_hosts? [y/N]" ADDHOSTS
-
-# Check the user's response
 if [[ $ADDHOSTS =~ ^[Yy]$ ]]; then
-  # Add github.com and bitbucket.org to ~/.ssh/known_hosts
+  # Add github.com and bitbucket.org to ~/.ssh/known_hosts (prints out several lines)
   ssh-keyscan github.com bitbucket.org >> ~/.ssh/known_hosts
-  echo "⏳ Updating SSH config"
+  #echo "⏳ Updating SSH config"
 fi
 
 # Symlink ~/.1password/agent.sock to the 1Password SSH agent socket that is tucked away in ~/Library
@@ -139,7 +138,6 @@ Host bitbucket.org$HOSTEXTENSION
   User git
   IdentityFile ~/.ssh/$FILENAME.pub
   IdentitiesOnly yes
---------------------
 
 _EOF
 
